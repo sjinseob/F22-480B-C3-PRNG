54551da004f85b3356dfdb6476eff18d1ca13a7b
SaraAlam <salam@colgate.edu>
2022-10-13 14:36:09 -0400

    generated keys

---
+++ task2_kg.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+char plain[] = "255044462d312e350a25d0d4c5d80a34";
+char cypher[] = "d06bf9d0dab8e8ef880660d2af65aa82";
+char IV[] = "09080706050403020100A2B2C2D2E2F2";
+
+void main(){
+    FILE *fp;
+
+    fp = fopen("keys.txt", "w+");
+    //fprintf(fp, "This is testing for fprintf...\n");
+   
+    char key[16]; 
+    int idx = 0;
+    for(int i = 1523999329; i <= 1524006529; i++){ //the 2 hour window on 2018 04 17 before 23:08:49  //1524006529
+        srand(i);
+        //printf("%d", i);
+        //printf("%s", key);
+        //printf("\n");
+        for(int j = 0; j< 16; j++){
+            key[j] = rand()%256;
+            //printf("%.2x", (unsigned char)key[j]);
+            fprintf(fp, "%.2x", (unsigned char)key[j]);
+    }
+    // Try the decrypt with the key here
+        fprintf(fp, "\n");
+    }
+    fclose(fp);
+}
