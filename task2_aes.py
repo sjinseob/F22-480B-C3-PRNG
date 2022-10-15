1812b09739bec7f6941f0cdb50798bb84bc71d5d
SaraAlam <salam@colgate.edu>
2022-10-15 13:38:33 -0400

    task2_aes some progress from 10/14-15

--- task2_aes.py
+++ task2_aes.py
@@ -14,28 +14,32 @@ def to_bin_of_str(utf8bin):
     for ch in utf8bin:
         s += chr(int(ch,2))
     return s
-keyDoc = open("test_keys.txt", 'r') # generates the 32-bit number?
-iv = b"09080706050403020100A2B2C2D2E2F2"  # generates the 16-bit number?
+keyDoc = open("keys.txt", 'r') # generates the 32-bit number?
+iv = b"0908070605040302" #0100A2B2C2D2E2F2"  # generates the 16-bit number?
 
 # plain = b"255044462d312e350a25d0d4c5d80a34"
 # cipher = b"d06bf9d0dab8e8ef880660d2af65aa82"
 
-plain = b"0123456789ABCDEF"
-
+plain = b"255044462d312e350a25d0d4c5d80a34"
+cipher = b"d06bf9d0dab8e8ef880660d2af65aa82"
+    
 for key in keyDoc.readlines():
     k = bytes(key.strip(), "ascii")
     aesCipher = Cipher(algorithms.AES(k),
                    modes.CBC(iv),
                    backend=default_backend())
-    aesEncryptor = aesCipher.encryptor()
+    #aesEncryptor = aesCipher.encryptor()
     aesDecryptor = aesCipher.decryptor()
-    #plaintext = aesDecryptor.update(cipher)
+    plaintext = aesDecryptor.update(cipher)
+    #for l in plaintext:
+       # plaintxt += chr(l)
     #print(plaintext)
-    cipher = aesEncryptor.update(plain)
-    recover = aesDecryptor.update(cipher)
-    if recover == plain:
+    #recover = aesDecryptor.update(cipher)
+    #print(plaintxt)
+    if plaintext == plain:
         print("[PASS]")
         print(key)
+        break
 
 '''cipher = aesEncryptor.update(plain)
 recover = aesDecryptor.update(cipher)
