0b8360b83abec7b2627bc9e0318b674ef182195f
SaraAlam <salam@colgate.edu>
2022-10-15 21:09:41 -0400

    task2_aes progress

--- task2_aes.py
+++ task2_aes.py
@@ -4,55 +4,43 @@ import os
 from binascii import unhexlify, hexlify   # Jin on 15 Oct: The binascii has a "unhexlify" function that can translate hex to binary.
 # from Crypto.Cipher import AES
 
-# key = os.urandom(32) # generates the 32-bit number?
-# print(len(key))
-# key2 = bytes("8dfa318cb2fbcaf11635475163586d90", "ascii")
-# print(type(key2))
-# print(len(key2))
-# iv = os.urandom(16)  # generates the 16-bit number?
+def main():
 
-'''
-# Jin on 15 Oct: I suspect we won't need this if „unhexlify" can perform the dirty work of transforming hex to binary. :)
-def to_bin_of_str(utf8bin):
-    s = ""
-    for ch in utf8bin:
-        s += chr(int(ch,2))
-    return s
-'''
-keyDoc = open("keys.txt", 'r') # generates the 32-bit number?
+    # keys generted by task2_kg.c
+    keyDoc = open("keys.txt", 'r')
 
-# iv = b"09080706050403020100A2B2C2D2E2F2"  # generates the 16-bit number?
-# plain = b"255044462d312e350a25d0d4c5d80a34"
-# cipher = b"d06bf9d0dab8e8ef880660d2af65aa82"
 
-# Jin on 15 Oct: „unhexlify" transforms our given hex values to binary
-plaintext = "255044462d312e350a25d0d4c5d80a34" 
-ciphertext = "d06bf9d0dab8e8ef880660d2af65aa82"
-iv = "09080706050403020100A2B2C2D2E2F2"
-plain_b = unhexlify(plaintext)
-cipher_b = unhexlify(ciphertext)
-iv_b = unhexlify(iv)
+    # Jin on 15 Oct: „unhexlify" transforms our given hex values to binary
+    # plaintext, ciphertext and iv from assignment pdf
+    plaintext = "255044462d312e350a25d0d4c5d80a34" 
+    ciphertext = "d06bf9d0dab8e8ef880660d2af65aa82"
+    iv = "09080706050403020100A2B2C2D2E2F2"
+    #convert the above into binary from hex format
+    plain_b = unhexlify(plaintext)
+    cipher_b = unhexlify(ciphertext)
+    iv_b = unhexlify(iv)
 
-f = open("task2_results.txt", "w")
+    # print all the different ciphertexts obtained using the keys from keys.txt
+    f = open("task2_results.txt", "w")
 
-for key in keyDoc.readlines():
-    k = unhexlify(key.strip())
+    # for each key in keys.txt, create an encryptor and get the ciphertext for the given plaintext
+    for key in keyDoc.readlines():
+        k = unhexlify(key.strip())
 
-    aesCipher = Cipher(algorithms.AES(k),
-                   modes.CBC(iv_b),
-                   backend=default_backend())
-    #aesEncryptor = aesCipher.encryptor()
-    aesDecryptor = aesCipher.decryptor()
-    cipher = aesEncryptor.update(plain_b)
-    f.write(str(hexlify(cipher)) + '\n')
-    
-    '''
-    recover = aesDecryptor.update(cipher)
-    if plain == recover:
-        print("[PASS]")
-    else:
-        print("plain",plain)
-        print("cipher",cipher.hex())
-        print("recover",recover)
-        print("[FAIL]")
-    '''
+        aesCipher = Cipher(algorithms.AES(k),
+                    modes.CBC(iv_b),
+                    backend=default_backend())
+        aesEncryptor = aesCipher.encryptor()
+        aesDecryptor = aesCipher.decryptor()
+        cipher = aesEncryptor.update(plain_b)
+        # if the given ciphertext matches the given ciphertext, the right key is found
+        if cipher_b == cipher:
+            print("[PASS]")
+            print(key)
+
+
+        f.write(str(hexlify(cipher)) + '\n')
+    f.close()
+
+if __name__=='__main__':
+    main()
